4a83530c4556146d390c030478e28c302088d504
AlecRuin <ruinhard@gmail.com>
2026-04-07 20:04:51 -0500

    fixed connection url

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -4,8 +4,8 @@ import { fileURLToPath } from "url"
 const __DIRNAME = dirname(fileURLToPath(import.meta.url))
 dotenv.config({path:join(__DIRNAME,"../../.env")})
 import mongoose from "mongoose";
-var DeadlabURI = (process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_DEADLAB_URI
-var SwoggerslolURI=(process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_SWOGGERSLOL_URI
+var DeadlabURI = (process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017/deadlab":process.env.MONGO_DEADLAB_URI
+var SwoggerslolURI=(process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017/swoggerslol":process.env.MONGO_SWOGGERSLOL_URI
 console.log("process.env.MONGO_SWOGGERSLOL_URI: ",SwoggerslolURI);
 console.log("process.env.MONGO_DEADLAB_URI: ",DeadlabURI);
 export const SWOGGERSLOL_CONNECTION = mongoose.createConnection(SwoggerslolURI)
