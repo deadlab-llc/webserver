8794847924cc5a3b717f6f4bec87cc686c416dac
AlecRuin <ruinhard@gmail.com>
2026-04-07 20:01:50 -0500

    updated connection logs

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -4,7 +4,9 @@ import { fileURLToPath } from "url"
 const __DIRNAME = dirname(fileURLToPath(import.meta.url))
 dotenv.config({path:join(__DIRNAME,"../../.env")})
 import mongoose from "mongoose";
-console.log("process.env.MONGO_SWOGGERSLOL_URI: ",process.env.MONGO_SWOGGERSLOL_URI);
-console.log("process.env.MONGO_DEADLAB_URI: ",process.env.MONGO_DEADLAB_URI);
-export const SWOGGERSLOL_CONNECTION = mongoose.createConnection((process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_SWOGGERSLOL_URI)
-export const DEADLAB_CONNECTION = mongoose.createConnection((process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_DEADLAB_URI)
+var DeadlabURI = (process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_DEADLAB_URI
+var SwoggerslolURI=(process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_SWOGGERSLOL_URI
+console.log("process.env.MONGO_SWOGGERSLOL_URI: ",SwoggerslolURI);
+console.log("process.env.MONGO_DEADLAB_URI: ",DeadlabURI);
+export const SWOGGERSLOL_CONNECTION = mongoose.createConnection(SwoggerslolURI)
+export const DEADLAB_CONNECTION = mongoose.createConnection(DeadlabURI)
