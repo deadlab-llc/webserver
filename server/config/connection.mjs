e7c80439f45d960305272d2acf3ec5e7b8d032aa
AlecRuin <ruinhard@gmail.com>
2026-04-07 19:53:20 -0500

    fixed docker not connecting via containers

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -6,5 +6,5 @@ dotenv.config({path:join(__DIRNAME,"../../.env")})
 import mongoose from "mongoose";
 console.log("process.env.MONGO_SWOGGERSLOL_URI: ",process.env.MONGO_SWOGGERSLOL_URI);
 console.log("process.env.MONGO_DEADLAB_URI: ",process.env.MONGO_DEADLAB_URI);
-export const SWOGGERSLOL_CONNECTION = mongoose.createConnection(process.env.MONGO_SWOGGERSLOL_URI)
-export const DEADLAB_CONNECTION = mongoose.createConnection(process.env.MONGO_DEADLAB_URI)
+export const SWOGGERSLOL_CONNECTION = mongoose.createConnection((process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_SWOGGERSLOL_URI)
+export const DEADLAB_CONNECTION = mongoose.createConnection((process.env.NODE_ENV==="production")?"mongodb://host.docker.internal:27017":process.env.MONGO_DEADLAB_URI)
