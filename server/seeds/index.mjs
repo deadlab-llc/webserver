f74d1ae7c3fbf536aa557e37e9314dfc54f4eab1
AlecRuin <ruinhard@gmail.com>
2026-04-07 20:59:03 -0500

    fixed seed script pt4

--- server/seeds/index.mjs
+++ server/seeds/index.mjs
@@ -1,9 +1,13 @@
 import Post from "./Posts.json" assert {type:"json"}
 import {SWOGGERSLOL_CONNECTION} from "../config/connection.mjs"
 import { POSTS } from "../schemas/index.mjs"
-await new Promise((resolve,reject)=>{
-    SWOGGERSLOL_CONNECTION.once("open",resolve)
-    SWOGGERSLOL_CONNECTION.once("error",reject)
-})
+if(SWOGGERSLOL_CONNECTION.readyState !== 1){
+    await new Promise((resolve, reject) => {
+        SWOGGERSLOL_CONNECTION.once("open", resolve)
+        SWOGGERSLOL_CONNECTION.once("error", reject)
+    })
+}
 await POSTS.deleteMany();
 await POSTS.insertMany(Post)
+console.log("Seeded successfully");
+process.exit(0)
