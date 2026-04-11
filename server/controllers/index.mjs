3f9a61c794aef399eb2a8d84b8a82eb357234efa
AlecRuin <ruinhard@gmail.com>
2026-04-11 15:56:35 -0500

    prettier fix

--- server/controllers/index.mjs
+++ server/controllers/index.mjs
@@ -4,7 +4,7 @@ import { ROUTES as DEADLAB_ROUTES } from "./deadlab.mjs";
 const ROUTES = Router();
 
 ROUTES.use((req, res, next) => {
-	if (req.hostname==="swoggerslol.com")return SWOGGERSLOL_ROUTES(req,res,next);
-	return DEADLAB_ROUTES(req,res,next)
+	if (req.hostname === "swoggerslol.com") return SWOGGERSLOL_ROUTES(req, res, next);
+	return DEADLAB_ROUTES(req, res, next);
 });
 export { ROUTES };
